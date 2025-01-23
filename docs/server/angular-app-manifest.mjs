ebe247aa402bad84e305479db20c74dff6bd0e01
Maria <mariaguydefontgalland@gmail.com>
2025-01-23 14:25:31 +0100

    (feat:) build du projet

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -13,7 +13,7 @@ export default {
   assets: {
     'index.csr.html': {size: 25612, hash: 'e348725ba15879b695111ac6cd6d8ee654d8b7eff81e8bcf6fc37eeec6c1b273', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 26128, hash: '7ba83587b6b1680ce2762c5533f267699107e0698269cceb708351604f3f8818', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 33049, hash: 'd09aff34f25e168801b894d439294f338bdba5dde1d42fa7e420b4634cf9b223', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.html': {size: 33040, hash: '456c2ba5bc2bb6d3758ee75366fa74dd49831bc14e9df268e97c10202d51765c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
