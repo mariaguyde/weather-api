6661c4beeaba7a5f66576468def160ae23c3aee0
Maria <mariaguydefontgalland@gmail.com>
2025-01-23 13:34:21 +0100

    (feat:) valeur de registrationStrategy changé lors de la déclaration du provideServiceWorker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,11 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), 
+              provideServiceWorker('ngsw-worker.js', 
+              {
+                enabled: !isDevMode(),
+                registrationStrategy: 'registerImmediately'
+              })
+              ]
 };
